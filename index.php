2dc82a61906a86b71d48143eb2b423320e95a0c0
pt1C3 <rafaelsilva1c3@gmail.com>
2022-03-27 15:44:22 +0100

    leaderboard the users linda mm
    sheesh

--- index.php
+++ index.php
@@ -16,16 +16,13 @@
     $games = $pdo->query('SELECT * FROM games ORDER BY rating DESC LIMIT 4;')->fetchAll();
 
     $BestRatedGames = $pdo->query('SELECT * FROM games ORDER BY rating DESC LIMIT 4;')->fetchAll();
-    $BestRatedUsers = $pdo->query('SELECT * FROM user ORDER BY rating DESC LIMIT 4;')->fetchAll();
+    $BestRatedUsers = $pdo->query('SELECT * FROM user WHERE isAdmin = 0 ORDER BY rating DESC LIMIT 5 ')->fetchAll();
     ?>
 
 
 
     <div class='data'>
-        <div style="display:flex;">
-            <div class='about'>
-                <h1>About us</h1>
-            </div>
+        <!--<div style="display:flex;">
             <div class='border'></div>
             <div class='games'>
                 <h1>Supported Games</h1>
@@ -38,26 +35,71 @@
                 </div>
                 <p style="margin-top: 5%">...and more!</p>
             </div>
-        </div>
-        <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-            echo '<p style="align-self: center;">Welcome ' . $_SESSION['name'] . '!</p>
-                <a class="play" href="play.php">Play</a>';
-        }
-        ?>
-
-
+        </div>-->
         <div>
-            <h1 style="text-align: left;margin-left:4%;font-size:25pt;">Best Rated Users</h1>
-            <div style="display: flex;padding:2%;gap:2%">
+<h1 style="text-align: left;width:100%;font-size:25pt;text-align:center;">User Leaderboard</h1>
+            <div class="containerTop" ><!--Container para o top 5 -->
+            <div class="userTop Top4">
+            <a href="profile.php?id=<?= $BestRatedUsers[0]['userID']?>">
+                <div>
+                    <img src="<?= $BestRatedUsers[0]['image']?>">
+                    <p><?= $BestRatedUsers[0]['username']?></p>
+                    <p style="font-size:12pt">Rating: <?= round($BestRatedUsers[0]['rating'],1)?></p>
+                    <p><b>4</b></p>
+                    
+                </div></a>
+            </div>
+            <div class="userTop Top2">
+            <a href="profile.php?id=<?= $BestRatedUsers[0]['userID']?>">
+                <div>
+                    <img src="<?= $BestRatedUsers[0]['image']?>">
+                    <p><?= $BestRatedUsers[0]['username']?></p>
+                    <p style="font-size:12pt">Rating: <?= round($BestRatedUsers[0]['rating'],1)?></p>
+                    <p><b>2</b></p>
+                    
+                </div></a>
+            </div>
 
-                <?php foreach ($BestRatedUsers as $BestRatedUser) : ?>
+            <div class="userTop Top1" >
+                <a href="profile.php?id=<?= $BestRatedUsers[0]['userID']?>">
+                <div>
+                    <img src="<?= $BestRatedUsers[0]['image']?>">
+                    <p><?= $BestRatedUsers[0]['username']?></p>
+                    <p style="font-size:12pt">Rating: <?= round($BestRatedUsers[0]['rating'],1)?></p>
+                    <p><b>1</b></p>
+                    
+                </div></a>
+            </div>
+
+            <div class="userTop Top3">
+            <a href="profile.php?id=<?= $BestRatedUsers[0]['userID']?>">
+                <div>
+                    <img src="<?= $BestRatedUsers[0]['image']?>">
+                    <p><?= $BestRatedUsers[0]['username']?></p>
+                    <p style="font-size:12pt">Rating: <?= round($BestRatedUsers[0]['rating'],1)?></p>
+                    <p><b>3</b></p>
+                    
+                </div></a>
+            </div>
+            <div class="userTop Top5">
+            <a href="profile.php?id=<?= $BestRatedUsers[0]['userID']?>">
+                <div>
+                    <img src="<?= $BestRatedUsers[0]['image']?>">
+                    <p><?= $BestRatedUsers[0]['username']?></p>
+                    <p style="font-size:12pt">Rating: <?= round($BestRatedUsers[0]['rating'],1)?></p>
+                    <p><b>5</b></p>
+                    
+                </div></a>
+            </div>
+                
+                <!--<?php foreach ($BestRatedUsers as $BestRatedUser) : ?>
 
                     <?php echo '<a href="profile.php?id=' . $BestRatedUser['userID'] . '"><img src="' . $BestRatedUser['image'] . '" class="userThumb"/>
                     <p class="rating" style="font-size:20pt;">' . $BestRatedUser['username'] . '</p>
-                    <p class="rating">Rating:' . $BestRatedUser['rating'] . '</p>
+                    <p class="rating">Rating:' . round($BestRatedUser['rating'],1) . '</p>
                     </a>' ?>
 
-                <?php endforeach; ?>
+                <?php endforeach; ?>-->
 
             </div>
         </div>
@@ -70,13 +112,18 @@
 
                     <?php echo '<a href="game.php?id=' . $BestRatedGame['gameID'] . '"><img src="' . $BestRatedGame['thumbnail'] . '" class="gameThumb"/>
                     <p class="rating" style="font-size:18pt;">' . $BestRatedGame['title'] . '</p>
-                    <p class="rating">Rating:' . $BestRatedGame['rating'] . '</p>
+                    <p class="rating">Rating:' . round($BestRatedGame['rating'],1) . '</p>
                     </a>' ?>
 
                 <?php endforeach; ?>
 
             </div>
         </div>
+        <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+            echo '<p style="align-self: center;">Welcome ' . $_SESSION['name'] . '!</p>
+                <a class="play" href="play.php">Play</a>';
+        }
+        ?>
 
 
 
