f8b13e0278d49a5b50e25066ff79624ae4258cf3
pt1C3 <rafaelsilva1c3@gmail.com>
2022-03-30 19:07:11 +0100

    comentario bonito e mudancas nas paginas de perfil

--- userProfile.php
+++ userProfile.php
@@ -50,70 +50,79 @@ include 'auth/verificarLogin.php';
                 <a href="?edit"><button class="btnEdit">Edit</button></a>
             </div>
         </div>
-            <div style="display:flex;height:60vh;  background-color: green;">
-                <div class="listasPerfil">
-                    <h1>Liked Games</h1>
+        <div style="display:flex;height:60vh;margin-bottom:10vh">
+            <div class="listasPerfil">
+                <h1>You like</h1>
 
-                    <div class="lista">
-                        <?php
-                        $gameIDs = $pdo->query('SELECT gameID FROM likedgames where userID= ' . $_SESSION["id"])->fetchAll();
-                        foreach ($gameIDs as $id) {
-                            $game = $pdo->query('SELECT * FROM games where gameID= ' . $id["gameID"])->fetch();
-                            echo '<a href="game.php?id=' . $game['gameID'] . '" class="itemLista">
+                <div class="lista">
+                    <?php
+                    $gameIDs = $pdo->query('SELECT gameID FROM likedgames where userID= ' . $_SESSION["id"])->fetchAll();
+                    foreach ($gameIDs as $id) {
+                        $game = $pdo->query('SELECT * FROM games where gameID= ' . $id["gameID"])->fetch();
+                        echo '<a href="game.php?id=' . $game['gameID'] . '" class="itemLista">
                             <img src="' . $game['thumbnail'] . '" style="height:100px;"/>
                             <p>' . $game['title'] . '</p>
                             </a>';
-                        }
+                    }
 
-                        ?>
-                    </div>
+                    ?>
                 </div>
-                <div class='border'></div>
-                <div class="listasPerfil">
-                    <h1 style="padding-left:2%">Follows</h1>
-                    <div class="lista">
-                        <?php
-                        /*                                                           |               |-> buscar o ID dos que são seguidos; |                             |-> por este utilizador*/
-                        $users = $pdo->query('SELECT * FROM user INNER JOIN follows on user.userID=follows.followedID where isAdmin = 0 and followerID=' . $_SESSION["id"])->fetchAll();
+            </div>
+            <div class='border'></div>
+            <div class="listasPerfil">
+                <h1 style="padding-left:2%">You follow</h1>
+                <div class="lista">
+                    <?php
+                    /*                                                           |               |-> buscar o ID dos que são seguidos; |                             |-> por este utilizador*/
+                    $users = $pdo->query('SELECT * FROM user INNER JOIN follows on user.userID=follows.followedID where isAdmin = 0 and followerID=' . $_SESSION["id"])->fetchAll();
 
-                        foreach ($users as $user) {
-                            $followAge = $pdo->query('SELECT DISTINCT DATE(followDate) as "followDate" FROM follows WHERE followedID=' . $user["userID"] . ' and followerID =' . $_SESSION["id"])->fetch();
-                            echo '<a href="profile.php?id=' . $user['userID'] . '" class="itemLista">
+                    foreach ($users as $user) {
+                        $followAge = $pdo->query('SELECT DISTINCT DATE(followDate) as "followDate" FROM follows WHERE followedID=' . $user["userID"] . ' and followerID =' . $_SESSION["id"])->fetch();
+                        echo '<a href="profile.php?id=' . $user['userID'] . '" class="itemLista">
                             <img src="' . $user['image'] . '" style="height:100px;"/>
                             <p>' . $user['username'] . ' - since ' . $followAge["followDate"] . '</p>
                             </a>';
-                        }
-
-                        ?>
-                    </div>
-                </div>
-
-            </div>
-            <div style="height:80vh;  background-color: green;">
-                <h1>Comentarios</h1>
-                <?php
-                $comments = $pdo->query("SELECT *  FROM comment where profileID=" . $_SESSION["id"])->fetchall();
-
-                if (count($comments) > 0) {
-                    foreach ($comments as $comment) {
-                        $author = $pdo->query('SELECT * FROM user WHERE userID=' . $comment["authorID"])->fetch();
-                        echo '<a href="./profile.php?id=' . $author["userID"] . '"><p style="border:solid 3pt yellow"><img src="' . $author["image"] . '" style="width:5vh"> ' . $author["username"] . ' - ' . $comment["shareDate"] . '</p></a>
-                                <p style="border:solid 3pt yellow">' . $comment["commentText"] . '</p>';
                     }
-                }
-                ?>
-                <br>
-                <form method="post" action="./auth/comment.php">
-                    <div style="margin-left:10%">
-                        <div style="height:50px;width:90%;vertical-align: middle;"><img style="height:44px;border:solid 3px black;border-image-source: linear-gradient(45deg,rgba(180, 0, 255, 1) 0%,rgba(0, 6, 255, 1) 50%,rgba(255, 0, 114, 1) 100%);border-image-slice: 100 0;border-image-slice: 1;" src="<?= $_SESSION["userAvatar"] ?>"><span style="margin-left:7pt;margin-bottom:7pt;"><?= $_SESSION["username"] ?></span></div><br>
-                        <input type="hidden" value="<?= $_SESSION["id"] ?>" name="authorID">
-                        <input type="hidden" value="<?= $_SESSION["id"] ?>" name="profileID">
 
-                        <label for="comment" style="width:100%">Comment:</label><br>
-                        <textarea name="comment" style="resize: none;width:90%;height:40%;"></textarea><br>
-                        <input style="margin-left:80%;width:10%;" type="submit">
-                    </div>
-                </form>
+                    ?>
+                </div>
+            </div>
+
+        </div>
+        <div style="height:auto;width:90%;margin-left:5%;">
+            <h1 style="width:inherit;margin-bottom:3vh">Comments</h1>
+            <?php
+            $comments = $pdo->query("SELECT *  FROM comment where profileID=" . $_SESSION["id"])->fetchall();
+
+            if (count($comments) > 0) {
+                foreach ($comments as $comment) {
+                    $author = $pdo->query('SELECT * FROM user WHERE userID=' . $comment["authorID"])->fetch();
+                    echo '
+                    <div class="comment" style="width:100%;height:auto;margin-bottom:3vh;background-color:rgba(0,0,0,0.5)">
+                        <div class="commentHead" style="display:flex;justify-content:space-between;margin-bottom:5pt;border-bottom:solid 1pt #1926da;">
+                        <a style="text-decoration:none" href="profile.php?id='. $comment["authorID"] .'">
+                            <img src="'. $author["image"].'" style="box-sizing: border-box;height:5vh;border:solid 2pt black;border-image-source: linear-gradient(45deg,rgba(180, 0, 255, 1) 0%,rgba(0, 6, 255, 1) 50%,rgba(255, 0, 114, 1) 100%);border-image-slice: 100 0;border-image-slice: 1;">
+                            <p style="display:inline">'.$author["username"].'</p>
+                        </a>
+                        <p style="display:inline">'. substr($comment["shareDate"],0,-3).'</p>
+                        </div>
+                        <p style="border:solid 2pt #1926da;border-top:transparent;border-right:transparent;white-space: pre;width:100%;box-sizing: border-box;height:auto;display:block">' . wordwrap($comment["commentText"], 115, "\n", true) . '</p>
+                    </div>';
+                }
+            }
+            ?>
+            <br>
+            <form method="post" action="./auth/comment.php">
+                <div style="margin-top:5vh;">
+                    <div style="height:50px;width:inherit;vertical-align: middle;"><img style="height:44px;border:solid 3pt black;border-image-source: linear-gradient(45deg,rgba(180, 0, 255, 1) 0%,rgba(0, 6, 255, 1) 50%,rgba(255, 0, 114, 1) 100%);border-image-slice: 100 0;border-image-slice: 1;" src="<?= $_SESSION["userAvatar"] ?>"><span style="margin-left:7pt;margin-bottom:7pt;"><?= $_SESSION["username"] ?></span></div><br>
+                    <input type="hidden" value="<?= $_SESSION["id"] ?>" name="authorID">
+                    <input type="hidden" value="<?= $_SESSION["id"] ?>" name="profileID">
+
+                    <label for="comment" style="width:100%">Comment:</label><br>
+                    <textarea name="comment" style="resize: none;width:100%;height:20vh;"></textarea><br>
+                    <input style="margin-left:90%;width:10%;" type="submit">
+                </div>
+            </form>
         </div>
     </div>
     <script>
