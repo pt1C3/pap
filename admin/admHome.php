f3666cca7f70f04b0b4de39e9950fc2f46067605
Nuno Costa <costanuno219@gmail.com>
2022-05-03 09:04:31 +0100

    adm statisticas online offline e ultimo mês logado

--- admin/admHome.php
+++ admin/admHome.php
@@ -16,6 +16,49 @@
   if (isset($_SESSION["adm"]) == false || $_SESSION["adm"] == false) header("location: ../index.php");
   include './navbar.php';
   include '../auth/db.php';
+
+ //Código para statisticas//
+
+
+ date_default_timezone_set('Europe/Lisbon');
+
+ $time10 = new DateTime(date("Y-m-d H:i:s"));
+ $time10 ->modify("- 10 second");// 10 segundos de diferença
+ $timeminus10 = $time10->format("Y-m-d H:i:s");
+ $on=0;
+ $off=0;
+ 
+ $lastActivity = $pdo->query('SELECT * FROM user')->fetchAll();
+ foreach ($lastActivity as $onliner) {
+   if($onliner["lastActivity"] > $timeminus10)
+ {
+    $on+=1;
+ }
+ 
+ }
+ 
+ foreach ($lastActivity as $offliner) {
+   if($offliner["lastActivity"] < $timeminus10)
+ {
+    $off+=1;
+ }
+ 
+ }
+ 
+ $timeMonth = new DateTime(date("Y-m-d H:i:s"));
+ $timeMonth ->modify("- 1 month");// 10 segundos de diferença
+ $timeminusMonth = $timeMonth->format("Y-m-d H:i:s");
+ $month=0;
+ 
+ foreach ($lastActivity as $onliner) {
+   if($onliner["lastActivity"] > $timeminusMonth)
+ {
+    $month+=1;
+ }
+ 
+ }
+
+
   ?>
   <div class="data" style="gap: 20px;">
     <div style="width:30%;height:100%;display:inline;background-color: #181b3f;border:3px solid blue;">
@@ -45,11 +88,16 @@
 
       <div id="items" style="display: flex;gap:25%;text-align:center;margin-left:5%">
 
-        <div id="statistics" style="margin:5%;">
+        <div id="statistics" style="margin:5%;width:500px;">
 
           <p style="font-size:30pt;font-family:Aldo;padding-bottom:40px">STATISTICS</p>
 
-          <p>People online right now: 69</p>
+
+          <p style="font-size: 20pt;">Online: <?= $on ?></p>
+
+          <p style="font-size: 20pt;">Offline: <?= $off?></p>
+
+          <p style="font-size: 20pt;"> Last Month: <?= $month?></p>
         </div>
 
         <div id="actions" style="margin:5%;">
@@ -64,7 +112,7 @@
 
           <a class="buttones" href="" style="width: 250px;">EDIT PROFILE</a>
 
-          <a class="buttones" href="" style="width: 250px;">EDIT PROFILE</a>
+          <a class="buttones" href="" style="width: 250px;">RESET PROFILE</a>
 
           </div>
 
