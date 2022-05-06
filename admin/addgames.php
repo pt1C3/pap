31a416f02cd7b84ff3159c24acc1e3f3b3fc3252
Nuno Costa <costanuno219@gmail.com>
2022-05-06 09:31:43 +0100

    add game css

--- admin/addgames.php
+++ admin/addgames.php
@@ -11,7 +11,7 @@
   <title>LetsGame</title>
 </head>
 
-<body >
+<body style="padding-top: 5%;">
   <?php
   session_start();
   if (isset($_SESSION["adm"]) == false || $_SESSION["adm"] == false) header("location: ../index.php");
@@ -21,19 +21,28 @@
 
 
   ?>
-  <div class="data" >
+  <div class="data">
 
-    <form class="formadd" action="processa/addsaga.php" method="post" style="gap: 20px;display:grid">
-        <p style="font-size:35pt;font-family:Aldo;text-align:center;">Add Game:</p>
-        <input type="text" name="title" placeholder="Title">  
-        <input type="text" name="rating" placeholder="Rating">
-        <input type="text" name="link" placeholder="Link">
-        <input type="text" name="thumbnail" placeholder="Thumbnail">
-        <input type="text" name="trailer" placeholder="Trailer">
-        <input type="text" name="genre" placeholder="Genre">
-        <input class="play" type="submit" value="Add">
 
+
+    <form class="formadd" action="auth/" method="post" style="gap: 20px;display:grid">
+      <p style="font-size:35pt;font-family:Aldo;text-align:center;">Add Game:</p>
+      <input type="text" name="title" placeholder="Title">
+      <input type="text" name="rating" placeholder="Rating">
+      <input type="text" name="link" placeholder="Link">
+      <input type="text" name="thumbnail" placeholder="Thumbnail">
+      <input type="text" name="trailer" placeholder="Trailer">
+      <input type="text" name="genre" placeholder="Genre">
+
+      <div style="display: flex;gap:5px;width:70%;margin-left:15%">
+        <a class="play" href="/pap/admin/admHome.php">
+          <img src="../images/arrow.png" alt="" style="width: 40%;transform:rotate(90deg);filter:invert(100%)">
+        </a>
+        <input class="play" type="submit" value="Add" style="margin-left: 0%;">
+
+      </div>
     </form>
+
 
   </div>
 
